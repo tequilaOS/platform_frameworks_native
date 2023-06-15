57fb80df6837a8450d1c124cba3981b6822c2b19
jimmyshiu <jimmyshiu@google.com>
2023-06-15 15:18:38 +0000

    Send load up hint for cache rendering
    cache buffer rendering supposes to utilize the cpu gap after current
    frame finised and before the next frame. But it could take long and
    delay the next frame start time and cause jank eventually.
    Sending CPU LOAD UP hint to prevent stealing time from the next frame.
    
    Bug: 285991802
    Test: PtsUiBench QHD
    Change-Id: Id619a8166ae253fef815a25a35fc4b82066144a9

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2586,6 +2586,7 @@ void SurfaceFlinger::composite(TimePoint frameTime, VsyncId vsyncId)
     ATRACE_FORMAT("%s %" PRId64, __func__, vsyncId.value);
 
     compositionengine::CompositionRefreshArgs refreshArgs;
+    refreshArgs.powerCallback = this;
     const auto& displays = FTL_FAKE_GUARD(mStateLock, mDisplays);
     refreshArgs.outputs.reserve(displays.size());
     std::vector<DisplayId> displayIds;
@@ -3962,6 +3963,10 @@ void SurfaceFlinger::triggerOnFrameRateOverridesChanged() {
     }();
 
     mScheduler->onFrameRateOverridesChanged(mAppConnectionHandle, displayId);
+}
+
+void SurfaceFlinger::notifyCpuLoadUp() {
+    mPowerAdvisor->notifyCpuLoadUp();
 }
 
 void SurfaceFlinger::initScheduler(const sp<const DisplayDevice>& display) {
